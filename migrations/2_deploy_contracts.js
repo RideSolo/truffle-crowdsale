68ff5995400ec8fbbf8de49fb9a0ad3b4ede1d17
Capital Technologies & Research <adrian@mycapitalco.in>
2018-06-28 21:21:21 +0300

    Main Network
    Contracts have been deployed on the Ethereum's Main Network:
    CapitalTechCrowdsale: 0x6992742729d11386f0efbf5fa5d195aa180b21b0
    
    CALLToken: 0x0c5d6e276f1b6d51e802343c0eb60ec876964f10
    
    CALLGToken: 0x1cb3d8997bc39667e9cbb2aa70203f94ecda1422

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,4 +1,3 @@
-const FiatContract = artifacts.require("./FiatContract.sol");
 const CALLGToken = artifacts.require("./CALLGToken.sol");
 const CALLToken = artifacts.require("./CALLToken.sol");
 const CapitalTechCrowdsale = artifacts.require("./CapitalTechCrowdsale.sol");
@@ -6,16 +5,13 @@ module.exports = function(deployer, network, accounts) {
   const wallet = accounts[0];
   return deployer
     .then(() => {
-      return deployer.deploy(FiatContract);
-    })
-    .then(() => {
       return deployer.deploy(CALLGToken);
     })
     .then(() => {
       return deployer.deploy(CALLToken);
     })
     .then(() => {
-      return deployer.deploy(CapitalTechCrowdsale, wallet, FiatContract.address, CALLToken.address, CALLGToken.address);
+      return deployer.deploy(CapitalTechCrowdsale, wallet, "0x8055d0504666e2B6942BeB8D6014c964658Ca591", CALLToken.address, CALLGToken.address);
     })
     .catch(console.error);
 };
--- truffle.js
+++ truffle.js
@@ -5,9 +5,7 @@ const MNEMONIC = process.env.MNEMONIC;
 const HDWalletProvider = require('truffle-hdwallet-provider');
 
 const NETWORK_IDS = {
-  ropsten: 2,
-  rinkeby: 4,
-  kovan: 42
+  mainnet: 1
 };
 
 module.exports = {
@@ -17,18 +15,6 @@ module.exports = {
       { test: /\.json$/i, loader: "json-loader"},
       { test: /\.sol/, loader: 'truffle-solidity' }
     ]
-  },
-  networks: {
-    ganache: {
-      host: "127.0.0.1",
-      port: 7545,
-      network_id: "5777",
-    },
-    test: {
-      host: "127.0.0.1",
-      port: 7545,
-      network_id: "5777",
-    }
   }
 };
 
@@ -36,7 +22,6 @@ for (let networkName in NETWORK_IDS) {
   module.exports.networks[ networkName ] = {
     provider: new HDWalletProvider(MNEMONIC, 'https://' + networkName + '.infura.io/' + INFURA_API_KEY),
     network_id: NETWORK_IDS[ networkName ],
-    //gas: 6000000,
     gasPrice: 50 * 1000000000 // 50 Gwei
   };
 }
